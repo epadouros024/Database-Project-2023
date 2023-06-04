25ecd20ec0798c7de1288b4c16ea13c7f2c496fa
kvchr <128921379+kvchr@users.noreply.github.com>
2023-06-04 19:11:24 +0300

    Create borrow_new.sql

---
+++ Dummy Data/borrow_new.sql
@@ -0,0 +1,116 @@
+#YYYY-MM-DD
+#date of borrow date of return
+#2023-06-02, 2023-06-09
+#2023-06-01, 2023-06-08
+#2023-05-31, 2023-06-07
+#2023-05-30, 2023-06-06
+#2023-05-29, 2023-06-05
+
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (1, 0, '2023-06-02', '2023-06-09', 21, 11, 21 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (2, 0, '2023-06-01', '2023-06-08', 21, 23, 21 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (3, 0, '2023-06-01', '2023-06-08', 22, 1, 22 );
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (4, 0, '2023-05-31', '2023-06-07, 21', 18, 31 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (5, 0, '2023-05-30', '2023-06-06', 23, 22, 33 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (6, 0, '2023-05-29', '2023-06-05', 23, 28, 33 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (7, 0, '2023-06-02', '2023-06-09', 24, 36, 34 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (8, 0, '2023-06-01', '2023-06-08', 24, 37, 34 );
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (9, 0, '2023-05-31', '2023-06-07', 21, 153, 10 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (10, 0, '2023-05-31', '2023-06-07', 21, 123, 12 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (11, 0, '2023-05-30',' 2023-06-06', 21, 154, 12 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (12, 0, '2023-05-30', '2023-06-06', 22, 82, 14 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (13, 0, '2023-05-31', '2023-06-07', 22, 85, 15 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (14, 0, '2023-06-01', '2023-06-08', 23, 86, 17 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (15, 0, '2023-06-01', '2023-06-08', 23, 121, 17 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (16, 0, '2023-05-31', '2023-06-07', 24, 87, 19 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (17, 0,' 2023-05-30','2023-06-06', 24, 124, 20 );
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (18, 0, '2023-06-01', '2023-06-08', 21, 111, 50 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (19, 0, '2023-05-30', '2023-06-06', 21, 161, 50 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (20, 0, '2023-05-31', '2023-06-07', 21, 162, 51 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (21, 0, '2023-05-31', '2023-06-07', 21, 195, 53 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (22, 0, '2023-06-02', '2023-06-09', 21, 194, 53 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (23, 0, '2023-06-02', '2023-06-09', 21, 153, 55 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (24, 0, '2023-05-31', '2023-06-07', 21, 154, 55 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (25, 0, '2023-05-31', '2023-06-07', 21, 215, 57 );
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (26, 0, '2023-05-29', '2023-06-05', 22, 217, 59 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (27, 0, '2023-05-29', '2023-06-05', 22, 222, 59 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (28, 0, '2023-05-30', '2023-06-06', 22, 177, 60 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (29, 0, '2023-06-01', '2023-06-08', 22, 177, 61 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (30, 0, '2023-05-30', '2023-06-06', 22, 170, 63 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (31, 0, '2023-05-30', '2023-06-06', 22, 170, 64 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (32, 0, '2023-06-01', '2023-06-08', 22, 137, 64 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (33, 0, '2023-06-01', '2023-06-08', 22, 137, 65 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (34, 0, '2023-06-02', '2023-06-09', 22, 141, 65 );
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (35, 0, '2023-05-29', '2023-06-05', 23, 103, 67 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (36, 0, '2023-05-29', '2023-06-05', 23, 106, 67 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (37, 0, '2023-05-30', '2023-06-06', 23, 106, 68 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (38, 0, '2023-05-30', '2023-06-06', 23, 120, 68 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (39, 0, '2023-05-30', '2023-06-06', 23, 120, 70 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (40, 0, '2023-06-01', '2023-06-08', 23, 120, 72 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (41, 0, '2023-05-31', '2023-06-07', 23, 86, 72 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (42, 0, '2023-05-31', '2023-06-07', 23, 78, 74 );
+
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (43, 0, '2023-05-30', '2023-06-06', 24, 87, 76 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (44, 0, '2023-05-30', '2023-06-06', 24, 88, 76 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (45, 0, '2023-05-29', '2023-06-05', 24, 50, 77 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (46, 0, '2023-05-29', '2023-06-05', 24, 59, 77 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (47, 0, '2023-05-29', '2023-06-05', 24, 50, 78 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (48, 0, '2023-06-01', '2023-06-08', 24, 21, 80 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (49, 0, '2023-06-02', '2023-06-09', 24, 143, 80 );
+insert into borrow (Borrow_ID, Status,Date_of_Borrow ,Day_of_Return, School_Admin_Id, Books_Book_Id, User_ID) 
+            values (50, 0, '2023-06-02', '2023-06-09', 24, 189, 82 );
+
