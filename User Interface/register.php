5bcfb41737b633b42b434c365cece35890fc0d33
epadouros024 <128921156+epadouros024@users.noreply.github.com>
2023-06-04 16:16:14 +0300

    Add files via upload

--- User Interface/register.php
+++ User Interface/register.php
@@ -38,18 +38,18 @@
 
         <label for="school">School:</label>
         <select name="school" id="school" required>
-            <option value="1">School 1</option>
-            <option value="2">School 2</option>
-            <option value="3">School 3</option>
-            <!-- Add more options as needed -->
-        </select><br><br>
+            <option value="1">Jefferson Middle School</option>
+            <option value="2">Lincoln High School</option>
+            <option value="3">New Arc High School</option>
+            <option value="3">Washington Elementary School</option>
+        </select><br><br>       
+            <label for="usertype">User Type:</label>
+        <select name="usertype" id="usertype" required>
+            <option value="0">Student</option>
+            <option value="1">Teacher</option>
+            <option value="2">Admin</option>
+            <option value="3">Administrator</option>
 
-        <label for="usertype">User Type:</label>
-        <select id="usertype" name="usertype">
-            <option value="1">Simple User</option>
-            <option value="2">Teacher</option>
-            <option value="3">Admin</option>
-            <option value="4">Administrator</option>
         </select><br><br>
 
 
@@ -65,19 +65,14 @@
 
 session_start();
 
-
-        // if (isset($_SESSION['user_id'])) {
-        //     $user_id = $_SESSION['user_id'];
-        //     // Now you can use $user_id in your script
-        // } else {
-        //     //The user is not logged in
-        //     header("Location: login.html"); /* Redirect browser */
-        //     }        
-
-
         $connection = mysqli_connect('localhost', 'root','', 'semester project 2');
         
         if (!$connection) {die("Connection failed: " . mysqli_connect_error());}
+
+
+
+
+
 
         if($stmt = $connection->prepare("INSERT INTO users (Username, Password, First_Name, Last_Name, Age, email, School_Id) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
             // Bind variables to the prepared statement
@@ -91,7 +86,6 @@ session_start();
             $lastname = $_POST['lastname'];
             $age = $_POST['age'];
             $school = $_POST['school'];
-            //$usertype = $_POST['usertype'];
             
             // Execute the prepared statement
             if($stmt->execute()) {
@@ -107,4 +101,50 @@ session_start();
         }
         
 
+
+
+
+
+
+// if(isset($_POST['register'])) {
+
+
+
+//     // Retrieve form data
+//     $username = $_POST['username'];
+//     $email = $_POST['email'];
+//     $password = $_POST['password'];
+//     $firstname = $_POST['firstname'];
+//     $lastname = $_POST['lastname'];
+//     $age = $_POST['age'];
+//     $school = $_POST['school'];
+    
+//     $insertQuery = "INSERT INTO users (Username, Password, First_Name, Last_Name,Age, email,School_Id) 
+//     VALUES ('$username', '$password', '$firstname', '$lastname', $age, '$email',$school)";
+
+//     // Perform necessary validation and processing
+//     // ... (e.g., check for duplicate usernames, validate email format, hash password)
+    
+//     // Save user data to database or perform other registration actions
+//     // ...
+//     //$insertQuery = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+//     if(mysqli_query($connection, $insertQuery)) {
+//         // Registration successful
+//         echo "The new user is: ";
+//         echo $username;
+//         header("Location: success.html");
+//         exit();
+//     } else {
+//         // Registration failed
+//         echo "Error: " . mysqli_error($connection);
+//     }
+    
+//     // Redirect the user to a success page or display a success message
+    
+//     echo "The new user is: ";
+//     echo $username;
+    
+//     //header("Location: login.html");
+//     exit();
+// }
 ?>
