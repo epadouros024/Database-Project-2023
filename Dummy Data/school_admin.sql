e3c0ac8e9194ab2bd1c68940b6faecbac431c023
kvchr <128921379+kvchr@users.noreply.github.com>
2023-06-04 19:21:42 +0300

    Create school_admin.sql

---
+++ Dummy Data/school_admin.sql
@@ -0,0 +1,12 @@
+insert into school_admin(School_Admin_Id, Name, Email)
+               values(21,'Yomama Moto','yomamamoto@gmail.com');
+
+insert into school_admin(School_Admin_Id, Name, Email)
+               values(22,'Jomama Koto','jomamaKoto@gmail.com');
+
+insert into school_admin(School_Admin_Id, Name, Email)
+               values(23,'Komama', 'Boto','komamaboto@gmail.com');
+
+insert into school_admin(School_Admin_Id, Name, Email)
+               values(24,'John', 'Foso','johnfoso@gmail.com');
+
